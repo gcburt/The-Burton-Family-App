e3c38ddb0db3f03f97cd6692f52fc5b38c81ed25
gcburt <grant.c.burt@gmail.com>
2025-08-17 12:13:04 -0400

    x

---
+++ The Burton Family App/GrantView.swift
@@ -0,0 +1,26 @@
+//
+//  GrantView.swift
+//  The Burton Family App
+//
+//  Created by Grant Burton on 8/15/25.
+//
+
+import SwiftUI
+
+struct GrantView: View {
+    
+    enum vehicle {
+        case car, plane
+    }
+    var number = 0
+    
+    
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+func count<T: Numeric>(number: T) -> T {
+    number + 1
+}
